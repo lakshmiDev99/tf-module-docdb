1849b89b0af8f61bc6a695f89960735dc94763be
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-09 13:10:45 +0530

    systems manger aws parameter

--- main.tf
+++ main.tf
@@ -8,9 +8,10 @@ resource "aws_docdb_subnet_group" "main" {
 resource "aws_docdb_cluster" "main" {
   cluster_identifier      = "${local.name_prefix}-cluster"
   engine                  = "docdb"
-  master_username         = "foo"
-  master_password         = "mustbeeightchars"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
+  master_username         = data.aws_ssm_parameter.master_username.value
+  master_password         = data.aws_ssm_parameter.master_password.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = var.skip_final_snapshot
 }
+
--- vars.tf
+++ vars.tf
@@ -1,3 +1,6 @@
 variable "env" {}
 variable "subnet_ids" {}
 variable "tags" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
